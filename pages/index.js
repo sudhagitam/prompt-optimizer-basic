2bf605aa08fe3a9a318f00986f13d9ef1eb2cd36
sudhagitam <sudhakarkatti@gmail.com>
2026-05-06 11:07:45 -0400

    update clear handle

--- pages/index.js
+++ pages/index.js
@@ -139,7 +139,7 @@ export default function Home() {
     }
     setTimeout(() => {
       setInput("");
-      //setResult("");
+      setResult("");
       setError("");
       setCopied(false);
       setLoading(false);
